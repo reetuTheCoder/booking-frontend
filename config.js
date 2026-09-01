27a527cf43e73adafc144b0689b5fed642f5dfc2
reetuTheCoder <codewithreetu@gmail.com>
2026-09-01 17:38:23 +0530

    save

---
+++ config.js
@@ -0,0 +1,7 @@
+window._APP_CONFIG_ = {
+  APP_NAME: "appv2",
+  API_KEY: "dXNlcjExX19TRVBSQVRPUl9fYXBwdjI=",
+  REGION: "US",
+  APP_VERSION: "V2",
+  API_BASE_URL: "https://deepmagic.onrender.com/api",
+};
